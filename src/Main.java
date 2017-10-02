ff1146abc9e9d56f338833316a80a90842992d2b
AsiaIv0918 <104051067@live.asia.edu.tw>
2017-10-02 15:29:17 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+//大小寫轉換
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        String str;
+        str=in.nextLine();
+        System.out.println(str.toLowerCase());
+    }
+}
